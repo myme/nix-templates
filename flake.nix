0718dcd9397e210ec29cb6866c669682c44081ac
Martin Myrseth <mm@myme.no>
2023-10-13 11:05:25 +0200

    Move more templates into templates attr

--- flake.nix
+++ flake.nix
@@ -13,16 +13,16 @@
         path = ./nodejs;
         description = "NodeJS based project";
       };
-    };
 
-    python = {
-      path = ./python;
-      description = "Python based project";
-    };
+      python = {
+        path = ./python;
+        description = "Python based project";
+      };
 
-    revealjs = {
-      path = ./revealjs;
-      description = "Build presentations in Reveal.js using Emacs Org Mode";
+      revealjs = {
+        path = ./revealjs;
+        description = "Build presentations in Reveal.js using Emacs Org Mode";
+      };
     };
 
   };
